23a9cc0979937a0edec63222b8ddddd73ee139bd
Pascal Erni <pascal.erni@outlook.com>
2013-12-22 21:22:55 +0100

    add dummy files for pushing empty folders

---
+++ parent/client/src/test/java/ch/infbr5/sentinel/client/DummyClass.java
@@ -0,0 +1,13 @@
+package ch.infbr5.sentinel.client;
+
+public class DummyClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
