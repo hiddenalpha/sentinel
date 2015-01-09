745b38dbb053d48e26fe2b43a19dc4e13124640e
Alexander Suter <alexander.suter@gmx.ch>
2015-01-09 18:35:18 +0100

    #49 Abhängigkeiten aktualisiert (mig-layout, junit, guava)

--- parent/client/src/main/java/ch/infbr5/sentinel/client/gui/components/journal/panel/GefechtsJournalModel.java
+++ parent/client/src/main/java/ch/infbr5/sentinel/client/gui/components/journal/panel/GefechtsJournalModel.java
@@ -12,80 +12,80 @@ import ch.infbr5.sentinel.common.util.DateFormater;
 
 public class GefechtsJournalModel extends AbstractTableModel {
 
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
 
-	private String[] columnNames = {"Datum", "Checkpoint", "Wer/Was/Wie/Wo", "Massnahmen", "Für wen?", "Erledigt"};
+   private final String[] columnNames = { "Datum", "Checkpoint", "Wer/Was/Wie/Wo", "Massnahmen", "FÃ¼r wen?", "Erledigt" };
 
-	private List<JournalGefechtsMeldung> meldungen;
+   private final List<JournalGefechtsMeldung> meldungen;
 
-	public GefechtsJournalModel(List<JournalGefechtsMeldung> meldungen) {
-		this.meldungen = meldungen;
-	}
+   public GefechtsJournalModel(final List<JournalGefechtsMeldung> meldungen) {
+      this.meldungen = meldungen;
+   }
 
-	public JournalGefechtsMeldung getItem(int row) {
-		return meldungen.get(row);
-	}
+   public JournalGefechtsMeldung getItem(final int row) {
+      return meldungen.get(row);
+   }
 
-	@Override
-	public String getColumnName(int column) {
-		return columnNames[column];
-	}
+   @Override
+   public String getColumnName(final int column) {
+      return columnNames[column];
+   }
 
-	@Override
-	public int getRowCount() {
-		return meldungen.size();
-	}
+   @Override
+   public int getRowCount() {
+      return meldungen.size();
+   }
 
-	@Override
-	public int getColumnCount() {
-		return columnNames.length;
-	}
+   @Override
+   public int getColumnCount() {
+      return columnNames.length;
+   }
 
-	public void add(JournalGefechtsMeldung m) {
-		this.meldungen.add(0, m);
-		fireTableRowsInserted(0, 0);
-	}
+   public void add(final JournalGefechtsMeldung m) {
+      this.meldungen.add(0, m);
+      fireTableRowsInserted(0, 0);
+   }
 
-	@Override
-	public Class<?> getColumnClass(int columnIndex) {
-		return String.class;
-	}
+   @Override
+   public Class<?> getColumnClass(final int columnIndex) {
+      return String.class;
+   }
 
-	@Override
-	public Object getValueAt(int rowIndex, int columnIndex) {
-		JournalGefechtsMeldung meldung = meldungen.get(rowIndex);
-		if (columnIndex == 0) {
-			return DateFormater.formatToDateWithTime(new Date(meldung.getMillis()));
-		}
-		if (columnIndex == 1) {
-			return meldung.getCheckpoint().getName();
-		}
-		if (columnIndex == 4) {
-			return PersonDetailsFormater.getFullName(meldung.getWeiterleitenAnPerson());
-		}
-		if (columnIndex == 2) {
-			return meldung.getWerWasWoWie();
-		}
-		if (columnIndex == 3) {
-			return meldung.getMassnahme();
-		}
-		if (columnIndex == 5) {
-			if (meldung.isIstErledigt()) {
-				return DateFormater.formatToDateWithTime(meldung.getZeitpunktErledigt());
-			}
-			return "";
-		}
-		return "na";
-	}
+   @Override
+   public Object getValueAt(final int rowIndex, final int columnIndex) {
+      final JournalGefechtsMeldung meldung = meldungen.get(rowIndex);
+      if (columnIndex == 0) {
+         return DateFormater.formatToDateWithTime(new Date(meldung.getMillis()));
+      }
+      if (columnIndex == 1) {
+         return meldung.getCheckpoint().getName();
+      }
+      if (columnIndex == 4) {
+         return PersonDetailsFormater.getFullName(meldung.getWeiterleitenAnPerson());
+      }
+      if (columnIndex == 2) {
+         return meldung.getWerWasWoWie();
+      }
+      if (columnIndex == 3) {
+         return meldung.getMassnahme();
+      }
+      if (columnIndex == 5) {
+         if (meldung.isIstErledigt()) {
+            return DateFormater.formatToDateWithTime(meldung.getZeitpunktErledigt());
+         }
+         return "";
+      }
+      return "na";
+   }
 
-	public void setGefechtsMeldungToDone(JournalGefechtsMeldung meldung) {
-		for (JournalGefechtsMeldung m : meldungen) {
-			if (m.getId() == meldung.getId()) {
-				m.setIstErledigt(true);
-				m.setZeitpunktErledigt(XMLGregorianCalendarConverter.dateToXMLGregorianCalendar(new Date()));
-				fireTableDataChanged();
-			}
-		}
-	}
+   public void setGefechtsMeldungToDone(final JournalGefechtsMeldung meldung) {
+      for (final JournalGefechtsMeldung m : meldungen) {
+         if (m.getId() == meldung.getId()) {
+            m.setIstErledigt(true);
+            m.setZeitpunktErledigt(XMLGregorianCalendarConverter.dateToXMLGregorianCalendar(new Date()));
+            fireTableDataChanged();
+         }
+      }
+   }
 
 }
